346ac5ad4e091ac61378ded161f41a3aa78e2dee
John Delong <jdelong15@hotmail.ca>
2019-08-20 17:49:27 -0400

    added routes for users, profiles, auth and posts

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 //test to make sure its running
 app.get("/", (req, res) => res.send("API running"));
 
+// defined routes 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 //looks for enviorment variable called PORT
 const PORT = process.env.PORT || 3000;
 
