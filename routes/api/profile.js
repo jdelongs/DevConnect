e2d39c21c7d3852be340d92e14dda43333b0c138
John Delong <jdelong15@hotmail.ca>
2019-08-27 20:38:41 -0400

    added the profile schema

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
 
-// @route  GET api/profile
-// @desc   Test route
-// @access Public 
-router.get('/', (req, res) => res.send('profile is working'));
+        if (!profile) {
+            return res.status(400).json({ msg: 'there is no profile for this user' });
+        }
+
+        res.json(profile);
+    } catch (err) {
+        console.err(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 
 module.exports = router; 
