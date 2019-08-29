6d8b44f31b165065674dfae8570fcc29caab7e52
John Delong <jdelong15@hotmail.ca>
2019-08-29 18:03:18 -0400

    created posts model

--- models/Profile.js
+++ models/Profile.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-const ProfileSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+const ProfileSchema = new Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
