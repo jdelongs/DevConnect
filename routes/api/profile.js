35dc2033f49ed032de6e756e985d9361c24f76ca
John Delong <jdelong15@hotmail.ca>
2019-08-29 17:28:40 -0400

    added github api call for user repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -275,6 +277,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Sever Error');
+    }
+});
+
+// @route  GET api/profile/github/:username
+// @desc   get user repos from Github
+// @access public
+
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if (response.statusCode !== 200) {
+                return res.status(404).send({ msg: 'No github profile found' });
+            }
+
+            res.json(JSON.parse(body));
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
     }
 });
 module.exports = router; 
