a992842356253262ed9bf3d79eeb00235eb40fc3
jdelongs <jdelong11@outlook.com>
2019-10-02 19:25:44 -0400

    added ability to create a profile for a user

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert"; 
-import Dashboard from "./components/dashboard/Dashboard"; 
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile"; 
 import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -39,6 +40,7 @@ const App = () => {
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/create-profile" component={CreateProfile} />
           </Switch>
         </section>
       </Fragment>
