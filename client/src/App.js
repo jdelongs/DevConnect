2614d9e228c7ec18a24ace015b01327910f34bca
jdelongs <jdelong11@outlook.com>
2019-10-06 21:14:55 -0400

    user profiles are now displayed with their skills, experience, education, and socials

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from "./components/routing/PrivateRoute";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile"; 
 
 // Redux
 import { Provider } from "react-redux";
@@ -45,6 +46,7 @@ const App = () => {
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/profiles" component={Profiles} />
+            <Route exact path="/profile/:id" component={Profile} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute exact path="/create-profile" component={CreateProfile} />
             <PrivateRoute exact path="/edit-profile" component={EditProfile} />
